c09efb00d3ce2d28ef5fa43ff522e813d5e2957d
K Briaan <kbriaan1@gmail.com>
2024-08-03 13:43:37 +0300

    fs

---
+++ app.js
@@ -0,0 +1,14 @@
+const { readFileSync, writeFileSync } = require('fs');
+const path = require('path');
+
+// Correct paths
+const firstPath = path.join(__dirname, 'content', 'first.txt');
+const secondPath = path.join(__dirname, 'content', 'second.txt');
+
+// Read the first file
+const first = readFileSync(firstPath, 'utf8');
+
+// Write to the second file
+writeFileSync(secondPath, first, 'utf8');
+
+console.log(first, "Written to second.txt");
