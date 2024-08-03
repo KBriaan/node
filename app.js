d8e0bb3a5d106dd55fee20d99267db33f97f1703
K Briaan <kbriaan1@gmail.com>
2024-08-03 13:17:22 +0300

    path

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
-//os  modules
+const path = require('path')
 
-const os = require('os')
-
-// info about current user
-const user = os.userInfo()
-console.log(user)
-
-//method returns the system uptime in seconds
-console.log(`the system uptime is ${os.uptime()}` )
-
-const currentOS ={
-    name:os.type(),
-    release:os.release(),
-    totalmem:os.totalmem(),
-    freemen:os.freemem(),
-
-}
-console.log(currentOS)
+console.log(path.sep)
+console.log(path.join)
--- modules.js
+++ modules.js
@@ -20,3 +20,24 @@ require('./grenade')
 sayHi('peter')
 sayHi(names.john)
 sayHi(names.susan)
+
+
+//3:os  modules
+
+const os = require('os')
+
+// info about current user
+const user = os.userInfo()
+console.log(user)
+
+//method returns the system uptime in seconds
+console.log(`the system uptime is ${os.uptime()}` )
+
+const currentOS ={
+    name:os.type(),
+    release:os.release(),
+    totalmem:os.totalmem(),
+    freemen:os.freemem(),
+
+}
+console.log(currentOS)
