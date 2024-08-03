7352a35d46e8f35524951335eed76c84e625694f
K Briaan <kbriaan1@gmail.com>
2024-08-03 13:03:44 +0300

    os

---
+++ modules.js
@@ -0,0 +1,22 @@
+//Globals -no window!!!
+
+//_dirname -path to current directory
+//_filename -file name
+//require -function to use modules
+//module-info about the current module
+//proces -info about where the program is being executed
+
+
+//Modules
+// COMON JSON, every file in node is a module
+//modules - these are encapsulated code
+
+
+const names = require('./names');
+const sayHi= require('./utils');
+const data =require ('./alternative')
+require('./grenade')
+
+sayHi('peter')
+sayHi(names.john)
+sayHi(names.susan)
