5862832b94ff2ad59e119e0bf9e52bef60841475
K Briaan <kbriaan1@gmail.com>
2024-08-03 13:09:47 +0300

    uptime

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 //os  modules
 
-const os = require('')
+const os = require('os')
+
+// info about current user
+const user = os.userInfo()
+console.log(user)
+
+//method returns the system uptime in seconds
+console.log(`the system uptime is ${os.uptime()}` )
