0420d3f1f8fab0b253b3b00e021544789f57d0cd
K Briaan <kbriaan1@gmail.com>
2024-08-03 13:48:08 +0300

    fs

--- modules.js
+++ modules.js
@@ -54,3 +54,21 @@ console.log(base)
 
 const absolute = path.resolve(__dirname, "content", "subfolder", "test.txt")
 console.log(absolute)
+
+
+//fs
+
+const { readFileSync, writeFileSync } = require('fs');
+const path = require('path');
+
+// Correct paths
+const firstPath = path.join(__dirname, 'content', 'first.txt');
+const secondPath = path.join(__dirname, 'content', 'second.txt');
+
+// Read the first file
+const first = readFileSync(firstPath, 'utf8');
+
+// Write to the second file
+writeFileSync(secondPath, first, 'utf8');
+
+console.log(first );
