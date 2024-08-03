e7f3b3c775f2d999bd3c9a6d8252cdba0fdff685
K Briaan <kbriaan1@gmail.com>
2024-08-03 12:09:13 +0300

    added path to directory to intro

--- intro.js
+++ intro.js
@@ -5,3 +5,11 @@ if (amount <10){
     console.log ("large number")
 }
 console.log("hello, this is my first node application")
+
+
+//_dirname -path to current directory
+console.log(__dirname)
+
+setInterval(()=>{
+console.log("Hello world")
+}, 10)
