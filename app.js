15cb1b917e0cccbccfcd4694af2907db869bb87d
K Briaan <kbriaan1@gmail.com>
2024-08-03 11:59:59 +0300

    node

---
+++ app.js
@@ -0,0 +1,13 @@
+//Globals -no window!!!
+
+//_dirname -path to current directory
+//_filename -file name
+//require -function to use modules
+//module-info about the current module
+//proces -info about where the program is being executed
+
+console.log(__dirname)
+
+setInterval(()=>{
+console.log("Hello world")
+}, 10)
