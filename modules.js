bd2ae66c20f8e6bf9ac366343cd1de462a7e6579
K Briaan <kbriaan1@gmail.com>
2024-08-03 13:31:00 +0300

    path module

--- modules.js
+++ modules.js
@@ -41,3 +41,16 @@ const currentOS ={
 
 }
 console.log(currentOS)
+
+
+//pathModule
+const path = require('path')
+
+const filePath =path.join('/content', 'subfolder','test.txt' )
+console.log(filePath)
+
+const base =path.basename(filePath)
+console.log(base)
+
+const absolute = path.resolve(__dirname, "content", "subfolder", "test.txt")
+console.log(absolute)
