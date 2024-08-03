e90098f0d47a1b5aba188caee89d32d3053d6960
K Briaan <kbriaan1@gmail.com>
2024-08-03 13:14:55 +0300

    current modeule

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ console.log(user)
 
 //method returns the system uptime in seconds
 console.log(`the system uptime is ${os.uptime()}` )
+
+const currentOS ={
+    name:os.type(),
+    release:os.release(),
+    totalmem:os.totalmem(),
+    freemen:os.freemem(),
+
+}
+console.log(currentOS)
